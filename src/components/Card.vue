85c9fca203b0da38e9c0fda19b90e6a78533bece
Anderson <74332377+MarshFfeco@users.noreply.github.com>
2023-04-16 20:02:26 -0300

    Speed Update

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,4 +1,7 @@
 <script setup>
+    import CardInformation from "./extra/CardInformation.vue"
+    import CardButton from "./extra/CardButton.vue";
+
     const props = defineProps({
         isProfessor: {
             type: Boolean,
@@ -6,45 +9,63 @@
         }
     })
 
+    const Student = [
+        {
+            "message": "Para Estudante do CEEP" , 
+            "information": "Para todos os estudantes"
+        },
+        {
+            "message": "Conteudo para estudantes" , 
+            "information": "Conteúdo postado pelos professores"
+        },
+    ]
+
+    const Professor = [
+        {
+            "message": "Para Professores do CEEP" , 
+            "information": "Para todos os professores"
+        },
+        {
+            "message": "Inserir Conteúdo" , 
+            "information": "Inserir conteúdo para os alunos"
+        },
+    ]
+
     const cardStyleModifier = props.isProfessor ? 'card-style dark' : 'card-style light'
     const buttonStyleModifier = props.isProfessor ? 'button-dark' : 'button-light'
     const dividerStyleModifier = props.isProfessor ? 'divider-dark' : 'divider-light'
-    const leftStyleModifier = props.isProfessor ? 'left text-dark' : 'left text-light'
+
+    const componentstyle = props.isProfessor ? 'information text-dark' : 'information text-light'
 </script>
 
 <template>
     <div class="container">
         <div :class=cardStyleModifier >
+            <div v-if=isProfessor class="tag">
+                <h4>Login necessário</h4>
+            </div>
+
             <div class="content">
                 <div class="title">
                     <h1>CEEP VALE DO PARAGUAÇU</h1>
 
                     <div>
                         <h3><slot name="type"></slot></h3>
-                        <h3>ACESSAR</h3>
+                        <h3>BETA</h3>
                     </div>
                 </div>
 
-                <div :class=buttonStyleModifier>
-                    <a href="#">CONTINUAR</a>
-                </div>
+                <CardButton :style=buttonStyleModifier />
 
                 <div :class=dividerStyleModifier></div>
 
-                <div class="footer">
-                    <div :class=leftStyleModifier>
-                        <v-icon name="px-check" animation = "wrench" />
-
-                        <p>comida</p>
-                    </div>
-
-                    <div class="right">
-                        <dfn>
-                            <abbr title="informação">
-                                <v-icon name="bi-info" animation="wrench" />
-                            </abbr>
-                        </dfn>
-                    </div>
+                <div class="cardInformation">
+                    <CardInformation 
+                        v-for="item in isProfessor ? Professor : Student"
+                        :style=componentstyle 
+                        :Message=item.message
+                        :Information=item.information
+                    />
                 </div>
             </div>
         </div>
@@ -53,14 +74,23 @@
 
 <style lang="scss" scoped>
     @import "../assets/scss/index.scss";
+    
+    .dark{ @apply bg-gray-900 text-gray-50 border-transparent }
+    .light{ @apply bg-white border-gray-200 text-gray-800; &:hover { @apply border-purple-600; }}
 
-    .text-dark { @apply text-gray-300 }
-    .text-light { @apply text-gray-600 }
+    .container { @apply h-full w-full max-w-full flex justify-center items-center; }
 
-    .dark{ @apply bg-gray-900 text-gray-50 }
-    .light{ @apply bg-white border-gray-200 text-gray-800 }
-    .container { @apply w-full h-full flex justify-center items-center; }
-    .card-style { @apply w-96 rounded-lg border border-solid pt-8 pb-4 pr-4 pl-4; }
+    .tag {
+        @apply w-36 text-center absolute px-4 py-2 bg-orange-400 text-gray-800 font-bold text-xs rounded-md uppercase 
+        -top-4 left-0 right-0 mx-auto; 
+    }
+    .card-style {
+         @apply w-96 rounded-lg border border-solid pt-8 pb-4 pr-4 pl-4 relative;
+
+         @media (max-width: 830px) {
+            @apply w-4/5;
+        }
+     }
     .content { @apply w-full flex flex-col gap-8; }
 
     .title {
@@ -87,39 +117,11 @@
             }
         }
     }
-
-    .button-light { 
-        @apply cursor-pointer transition-colors w-full text-center font-bold px-8 py-4 border border-solid border-gray-800; 
-        
-        &:hover {
-            @apply bg-gray-800 text-white;
-        }
-
-        &:active {
-            @apply bg-gray-800 text-white;
-        } 
-    }
-
-    .button-dark { 
-        @apply cursor-pointer transition-colors w-full text-center font-bold px-8 py-4 border border-solid 
-        border-transparent bg-purple-600; 
-        
-        &:hover {
-            @apply bg-purple-700 text-white;
-        }
-
-        &:active {
-            @apply bg-purple-700 text-white;
-        } 
-    }
-
+    
     .divider-light { @apply w-full h-0 border border-solid border-gray-200; }
     .divider-dark { @apply w-full h-0 border border-solid border-gray-700; }
 
-    .footer {
-        @apply flex flex-row justify-between;
-
-        .left {@apply flex flex-row}
-        .right {cursor: pointer;}
+    .cardInformation {
+        @apply flex flex-col gap-4;
     }
 </style>
---
+++ src/components/extra/CardButton.vue
@@ -0,0 +1,41 @@
+<script setup>
+    const props = defineProps({
+        style: {
+            type: String,
+            required: true
+        }
+    })
+</script>
+
+<template>
+    <div :class=style>
+        <a href="#">CONTINUAR</a>
+    </div>
+</template>
+
+<style scoped lang="scss">
+    .button-light { 
+        @apply cursor-pointer transition-colors w-full text-center font-bold px-8 py-4 border border-solid border-gray-800; 
+        
+        &:hover {
+            @apply bg-gray-800 text-white;
+        }
+
+        &:active {
+            @apply bg-gray-800 text-white;
+        } 
+    }
+
+    .button-dark { 
+        @apply cursor-pointer transition-colors w-full text-center font-bold px-8 py-4 border border-solid 
+        border-transparent bg-purple-500; 
+        
+        &:hover {
+            @apply bg-purple-700 text-white;
+        }
+
+        &:active {
+            @apply bg-purple-700 text-white;
+        } 
+    }
+</style>
